4d5aecc5816812b0aced8365bd2b7b225c9fe05c
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-08-03 03:27:52 -0300

    tests: ensure should return an error if name is empty

---
+++ app/domain/entities/client_test.go
@@ -0,0 +1,37 @@
+package entities
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClient(t *testing.T) {
+
+	t.Run("should return an error if name is empty", func(t *testing.T) {
+		c, err := NewClient(1, "", Male, 29)
+
+		assert.Error(t, err)
+		assert.Nil(t, c)
+		assert.Equal(t, "name is required", err.Error())
+
+	})
+
+	t.Run("should return an error if name have spaces", func(t *testing.T) {
+
+		c, err := NewClient(1, "         ", Female, 25)
+
+		assert.Error(t, err)
+		assert.Nil(t, c)
+		assert.Equal(t, "name is required", err.Error())
+	})
+
+	t.Run("should return an error if name have less than 5 characters", func(t *testing.T) {
+
+		c, err := NewClient(1, "taok", Male, 20)
+
+		assert.Error(t, err)
+		assert.Nil(t, c)
+		assert.Equal(t, "name must have at least 5 characters", err.Error())
+	})
+}
