b5c913e4e495beaf062442487ada3d7f0b41a842
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-08-03 03:28:19 -0300

    tests: ensure should return an error if name only spaces

--- app/domain/entities/client_test.go
+++ app/domain/entities/client_test.go
@@ -17,7 +17,7 @@ func TestClient(t *testing.T) {
 
 	})
 
-	t.Run("should return an error if name have spaces", func(t *testing.T) {
+	t.Run("should return an error if name have only spaces", func(t *testing.T) {
 
 		c, err := NewClient(1, "         ", Female, 25)
 
@@ -26,12 +26,4 @@ func TestClient(t *testing.T) {
 		assert.Equal(t, "name is required", err.Error())
 	})
 
-	t.Run("should return an error if name have less than 5 characters", func(t *testing.T) {
-
-		c, err := NewClient(1, "taok", Male, 20)
-
-		assert.Error(t, err)
-		assert.Nil(t, c)
-		assert.Equal(t, "name must have at least 5 characters", err.Error())
-	})
 }
