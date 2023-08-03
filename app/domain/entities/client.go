9484d1cfbf8ca53af51d177d78ec52901be32206
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-08-03 03:27:23 -0300

    Client entity

---
+++ app/domain/entities/client.go
@@ -0,0 +1,47 @@
+package entities
+
+import (
+	"errors"
+	"strings"
+)
+
+type Gender int
+
+const (
+	Female Gender = iota
+	Male
+)
+
+type Client struct {
+	ID     int
+	Name   string
+	Gender Gender
+	Age    int
+}
+
+// NewClient creates a new Client, validating that the provided name, gender and age are valid.
+func NewClient(id int, name string, gender Gender, age int) (*Client, error) {
+	name = strings.TrimSpace(name)
+	if name == "" {
+		return nil, errors.New("name is required")
+	}
+
+	if len(name) < 5 {
+		return nil, errors.New("name must have at least 5 characters")
+	}
+
+	if gender != Male && gender != Female {
+		return nil, errors.New("For now we have only two genders, sorry. We Will fix it soon")
+	}
+
+	if age < 0 || age > 120 {
+		return nil, errors.New("age must be between 0 and 120")
+	}
+
+	return &Client{
+		ID:     id,
+		Name:   name,
+		Gender: gender,
+		Age:    age,
+	}, nil
+}
