f2aa9c3cecc270739a5b0e9c72be784ad8b61a26
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-08-06 03:40:59 -0300

    feat: CreateClient usecase interface

---
+++ app/domain/interfaces/usecases/create_client_interface.go
@@ -0,0 +1,8 @@
+package usecases
+
+import "github.com/palexandremello/ramenshop-backend/app/domain/entities"
+
+// CreateClient usecase interface
+type CreateClient interface {
+	Create(id int, name string, gender entities.Gender, age int) (*entities.Client, error)
+}
