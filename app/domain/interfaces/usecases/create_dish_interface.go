419f7466f569cd6d0f90037067ffb8cf4a6ff3ae
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-08-10 11:45:52 -0300

    feat: CreateDish UseCase

--- app/domain/interfaces/usecases/create_dish_interface.go
+++ app/domain/interfaces/usecases/create_dish_interface.go
@@ -5,19 +5,3 @@ import "github.com/palexandremello/ramenshop-backend/app/domain/entities"
 type CreateDish interface {
 	Create(name string, description string, photo *entities.Photo) (*entities.Dish, error)
 }
-
-type UpdateDish interface {
-	UpdateDish(id int, name string, description string, photo *entities.Photo) (*entities.Dish, error)
-}
-
-type DeleteDish interface {
-	Delete(id int) error
-}
-
-type ListDishes interface {
-	List() ([]entities.Dish, error)
-}
-
-type GetDishByID interface {
-	GetByID(id int) (*entities.Dish, error)
-}
--- app/domain/usecases/create_dish.go
+++ app/domain/usecases/create_dish.go
@@ -1,6 +1,9 @@
 package usecases
 
 import (
+	"errors"
+
+	"github.com/palexandremello/ramenshop-backend/app/domain/entities"
 	"github.com/palexandremello/ramenshop-backend/app/domain/interfaces/repositories"
 )
 
@@ -8,6 +11,26 @@ type dishUseCaseImpl struct {
 	dishRepo repositories.DishRepository
 }
 
-func NewDishUsecase(repo repositories.DishRepository) *dishUseCaseImpl {
+func NewDishUseCase(repo repositories.DishRepository) *dishUseCaseImpl {
 	return &dishUseCaseImpl{dishRepo: repo}
+}
+
+func (du *dishUseCaseImpl) Create(name string, description string, photo *entities.Photo) (*entities.Dish, error) {
+	if name == "" {
+		return nil, errors.New("name is required")
+	}
+
+	dish := &entities.Dish{
+		Name:        name,
+		Description: description,
+		Photo:       photo,
+	}
+
+	err := du.dishRepo.Save(dish)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return dish, nil
 }
