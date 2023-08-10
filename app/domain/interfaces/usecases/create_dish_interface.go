3b390e6172e1530f6cf83f2082fb20206d9461b0
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-08-10 16:35:04 -0300

    refactor: Refactoring CreateDish UseCase interface

--- app/domain/interfaces/usecases/create_dish_interface.go
+++ app/domain/interfaces/usecases/create_dish_interface.go
@@ -2,6 +2,7 @@ package usecases
 
 import "github.com/palexandremello/ramenshop-backend/app/domain/entities"
 
+// CreateDish UseCase interface
 type CreateDish interface {
 	Create(name string, description string, photo *entities.Photo) (*entities.Dish, error)
 }
