67c03e8ce0f79444e5204ab6008825bd509d5193
Paulo Mello <palexandremello@gmail.com>
2023-08-13 00:15:24 -0300

    tests: should not add items to a closed order

--- app/domain/usecases/add_order_items_test.go
+++ app/domain/usecases/add_order_items_test.go
@@ -2,6 +2,7 @@ package usecases
 
 import (
 	"testing"
+	"time"
 
 	"github.com/palexandremello/ramenshop-backend/app/domain/entities"
 	repomocks "github.com/palexandremello/ramenshop-backend/app/domain/interfaces/repositories/mocks"
@@ -30,5 +31,24 @@ func TestAddOrderItems(t *testing.T) {
 		assert.Nil(t, err)
 		assert.Len(t, order.Items, 1)
 
+	})
+
+	t.Run("should not add items to a closed order", func(t *testing.T) {
+		now := time.Now()
+		mockOrderRepo := new(repomocks.MockOrderRepository)
+		usecase := NewAddOrderItems(mockOrderRepo)
+		closedOrder := &entities.Order{ID: 1, ClosedAt: &now}
+		newItems := []entities.OrderItem{
+			{OrderID: 1, Dish: entities.Dish{
+				ID:   1,
+				Name: "Ramen",
+			}},
+		}
+		mockOrderRepo.On("GetOrder", 1).Return(closedOrder, nil)
+
+		err := usecase.Add(1, newItems)
+
+		assert.NotNil(t, err)
+		assert.Equal(t, "cannot add items to a closed order", err.Error())
 	})
 }
