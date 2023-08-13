cc5f3e77162d87d761c6489618d622b0d4b8424f
Paulo Mello <palexandremello@gmail.com>
2023-08-13 04:03:25 -0300

    feat: CreateClientController implementation

---
+++ app/application/controllers/create_client.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"fmt"
+
+	"github.com/palexandremello/ramenshop-backend/app/domain/entities"
+	"github.com/palexandremello/ramenshop-backend/app/domain/interfaces/controllers"
+	"github.com/palexandremello/ramenshop-backend/app/domain/interfaces/usecases"
+)
+
+type CreateClientController struct {
+	creteClientUseCase usecases.CreateClient
+}
+
+var _ controllers.CreateClientController = &CreateClientController{}
+
+func NewCreateClientController(useCase usecases.CreateClient) controllers.CreateClientController {
+	return &CreateClientController{creteClientUseCase: useCase}
+}
+
+func (cc *CreateClientController) Create(client *entities.Client) (*entities.Client, error) {
+	createdClient, err := cc.creteClientUseCase.Create(client.ID, client.Name, client.Gender, client.Age)
+
+	if err != nil {
+		return nil, fmt.Errorf("could not ceate a consumer %w", err)
+	}
+	return createdClient, nil
+}
