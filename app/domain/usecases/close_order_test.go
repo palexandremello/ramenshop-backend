5ddfdbd5fb7be7fc4b318967effbb8aae4ba867b
Paulo Mello <palexandremello@gmail.com>
2023-08-13 01:27:19 -0300

    tests: should return an error if order already closed

--- app/domain/usecases/close_order_test.go
+++ app/domain/usecases/close_order_test.go
@@ -2,6 +2,7 @@ package usecases
 
 import (
 	"testing"
+	"time"
 
 	"github.com/palexandremello/ramenshop-backend/app/domain/entities"
 	repomocks "github.com/palexandremello/ramenshop-backend/app/domain/interfaces/repositories/mocks"
@@ -26,5 +27,22 @@ func TestCloseOrder(t *testing.T) {
 
 		assert.Nil(t, err)
 		assert.NotNil(t, order.ClosedAt)
+	})
+
+	t.Run("should return an error if order already closed", func(t *testing.T) {
+		mockOrderRepo := new(repomocks.MockOrderRepository)
+		usecase := NewCloseOrder(mockOrderRepo)
+
+		now := time.Now()
+		order := &entities.Order{
+			ID:       1,
+			ClosedAt: &now,
+		}
+		mockOrderRepo.On("GetOrder", 1).Return(order, nil)
+
+		err := usecase.Execute(1)
+
+		assert.NotNil(t, err)
+		assert.Equal(t, "order was closed already", err.Error())
 	})
 }
