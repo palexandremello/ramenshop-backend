dcdc67a67d8f7c1cc8fd2e1b4788bbf186d1a6a9
Paulo Mello <palexandremello@gmail.com>
2023-08-13 04:16:24 -0300

    tests: should return an error if CreateClient usecase fails

--- app/application/controllers/create_client_test.go
+++ app/application/controllers/create_client_test.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"errors"
 	"testing"
 
 	controllermocks "github.com/palexandremello/ramenshop-backend/app/application/controllers/mocks"
@@ -9,14 +10,15 @@ import (
 )
 
 func TestCreateClientController(t *testing.T) {
-	mockUseCase := new(controllermocks.CreateClient)
-	controller := NewCreateClientController(mockUseCase)
-	client := &entities.Client{
-		Name:   "Jeff Buckley",
-		Gender: entities.Male,
-		Age:    30,
-	}
 	t.Run("should create a client with successfull", func(t *testing.T) {
+		mockUseCase := new(controllermocks.CreateClient)
+		controller := NewCreateClientController(mockUseCase)
+		client := &entities.Client{
+			Name:   "Jeff Buckley",
+			Gender: entities.Male,
+			Age:    30,
+		}
+
 		mockUseCase.On("Create", client.ID, client.Name, client.Gender, client.Age).Return(client, nil)
 
 		result, err := controller.Create(client)
@@ -24,5 +26,24 @@ func TestCreateClientController(t *testing.T) {
 		assert.NoError(t, err)
 		assert.NotNil(t, result)
 		assert.Equal(t, client.Name, result.Name)
+	})
+
+	t.Run("should return an error if CreateClient usecase fails", func(t *testing.T) {
+		mockUseCase := new(controllermocks.CreateClient)
+		controller := NewCreateClientController(mockUseCase)
+		client := &entities.Client{
+			Name:   "Jeff Buckley",
+			Gender: entities.Male,
+			Age:    30,
+		}
+
+		mockErr := errors.New("any_error")
+
+		mockUseCase.On("Create", client.ID, client.Name, client.Gender, client.Age).Return(nil, mockErr)
+
+		_, err := controller.Create(client)
+
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "any_error")
 	})
 }
--- app/application/controllers/mocks/create_client_usecase_mock.go
+++ app/application/controllers/mocks/create_client_usecase_mock.go
@@ -11,5 +11,8 @@ type CreateClient struct {
 
 func (m *CreateClient) Create(id int, name string, gender entities.Gender, age int) (*entities.Client, error) {
 	args := m.Called(id, name, gender, age)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
 	return args.Get(0).(*entities.Client), args.Error(1)
 }
