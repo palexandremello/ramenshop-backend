da88a9192cf54a69709eaa1913889ba96a4d7fc5
Paulo Mello <palexandremello@gmail.com>
2023-08-15 19:01:12 -0300

    feat: TableRepositoryMock class

---
+++ app/domain/interfaces/repositories/mocks/table_repository_mock.go
@@ -0,0 +1,35 @@
+package repomocks
+
+import (
+	"github.com/palexandremello/ramenshop-backend/app/domain/entities"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockTableRepository struct {
+	mock.Mock
+}
+
+// FindByID implements repositories.TableRepository.
+func (*MockTableRepository) FindByID(id int) (*entities.Table, error) {
+	panic("unimplemented")
+}
+
+// List implements repositories.TableRepository.
+func (*MockTableRepository) List() ([]*entities.Table, error) {
+	panic("unimplemented")
+}
+
+// Remove implements repositories.TableRepository.
+func (*MockTableRepository) Remove(id int) error {
+	panic("unimplemented")
+}
+
+// Update implements repositories.TableRepository.
+func (*MockTableRepository) Update(table *entities.Table) error {
+	panic("unimplemented")
+}
+
+func (m *MockTableRepository) Add(table *entities.Table) (*entities.Table, error) {
+	args := m.Called(table)
+	return args.Get(0).(*entities.Table), args.Error(1)
+}
