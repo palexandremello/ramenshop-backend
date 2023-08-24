fbcc0ed1f6eaeaca4c89de00a6b43ea66e869f83
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-08-24 01:34:47 -0300

    feat: Infra db

---
+++ app/infra/db/postegresql/connection.go
@@ -0,0 +1,36 @@
+package postegresql
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func InitializeDatabase() *sql.DB {
+
+	host := "172.18.0.1"
+	port := 5432
+	user := "postgres"
+	password := "postgres"
+	dbname := "postgres"
+
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	fmt.Println(connStr)
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Failed to ping database: %v", err)
+	}
+
+	log.Println("Successfully connected to database")
+
+	return db
+}
